431fdfdd15e5c501016ea8e89eaaf71db56ebb27
nishchit pro coder <99675062+Nishchit-pro-coder@users.noreply.github.com>
2022-05-04 12:30:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+
+var canvas = new fabri.canvas('myCanvas')
+
+ball_y=0
+ball_x=0
+hole_y=400
+hole_x=800
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	fabric.Image.fromURL("ball.png", function(img) {
+        hole_obj = img;
+                           
+        hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+        hole_obj.set({
+            left:hole_y,
+            left:hole_x
+        });
+        canvas.add(ball_obj);
+    });
+	new_image()
+}
+
+function new_image()
+{
+	fabric.Image.fromURL("ball.png", function(img) {
+        ball_obj = img;
+                           
+        ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+        ball_obj.set({
+            left:ball_y,
+            left:ball_x
+        });
+        canvas.add(ball_obj);
+    });
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if ((ball_x==hole_x)&&(ball_y==hole_y))
+	     canvas.remove(ball_obj)
+	
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	
+	function up()
+	{
+		if (ball_yy >=0)
+    {
+        ball_y = ball_y - block_image_height;
+
+        console.log("when up arrow is pressed, x = " + ball_x + " | y = " +ball_y);
+		canvas.add(ball_obj);
+		new_image() 
+    }
+		// Write a code to move ball upward.
+	}
+
+	function down()
+	{
+		if (ball_y >=500)
+    {
+        ball_y = ball_y + block_image_height;
+
+        console.log("when down arrow is pressed, x = " + ball_x + " | y = " +ball_y);
+		canvas.add(ball_obj);
+        new_image()
+	}
+		 // Write a code to move ball downward.
+	}
+
+	function left()
+	{
+		if(ball_x >5)
+		{
+			ball_x = ball_x - block_image_width;
+		
+			console.log("when left arrow is pressed, x = " + ball_x + " | y = " +ball_y);
+			canvas.add(ball_obj);
+			new_image() 
+			// Write a code to move ball left side.
+		}
+	}
+
+	function right()
+	{
+		if(ball_x <=1050)
+		{
+			ball_x = ball_x    + block_image_width	;
+
+        console.log("when right arrow is pressed, x = " + ball_x + " | y = " +ball_y);
+		canvas.add(ball_obj);
+        new_image()
+			// Write a code to move ball right side.
+		}
+	}
+
